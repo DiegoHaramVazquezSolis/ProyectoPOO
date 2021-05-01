9c1575d8844c99b17553db7c25531b5ebe57544a
david12343564 <david_emmi@hotmail.com>
2021-04-30 20:09:26 -0500

    Se crea clase Ticket con sus atributos

---
+++ ProyectoPOO/src/clases/Ticket.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class Ticket {
+	
+private Producto [] productos;
+    
+    private Cliente cliente;
+    
+    private double subtotal, impuesto, total;
+    
+    private String folio;
+    
+    private Usuario usuario;
+}
