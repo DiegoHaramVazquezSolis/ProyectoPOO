11f252833cd5f8a98225f86f598b8822a6586852
david12343564 <david_emmi@hotmail.com>
2021-05-10 11:45:32 -0500

    Update Cliente.java
    Se agregan el metodo equals a cliente

--- ProyectoPOO/src/clases/Cliente.java
+++ ProyectoPOO/src/clases/Cliente.java
@@ -21,4 +21,26 @@ public class Cliente extends Persona{
     public String getCid() {
         return id;
     }
+    
+    public boolean equals(Cliente c)
+    {
+    if (!(c instanceof Cliente)) {
+            return false;
+        }
+        
+    else{
+        Cliente client = (Cliente) c;
+        boolean flag1,flag2,flag3,flag4,flag5;
+        flag1= this.getNombrePer()== c.getNombrePer();
+        flag2= this.getRfc()== c.getRfc();
+        flag3= this.getTelefono()== c.getTelefono();
+        flag4= this.getDomicilio()==c.getDomicilio();
+        flag5= this.getCid()==c.getCid();
+        
+           if (flag1 && flag2 && flag3 && flag4 && flag5) {
+            return true;
+        }
+    }
+    return false;
+    }
 }
